4ff9e6955d9d7ffe31838dd1773489ae747513ff
Rosel Padilla <roselpadilla.mail@gmail.com>
2015-07-13 17:34:08 -0400

    index card is working and data placement is correct

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+    var index = 0;
+    showQnA(index);
 
-    var deck = [];
-
-    $(".editQuestion").on("click", function(){
-      $(".theAnswer").attr("style", "visibility: hidden")
-
-      var theQuestion = prompt("SET YOUR QUESTION");
-      $(".theQuestion").html(theQuestion + "\n");
-
-      var theAnswer = prompt("ANSWER TO THE QUESTION");
-      $(".theAnswer").html("\n" + theAnswer);
-    });
-
-    $(".showAnswer").on("click", function(){
-      $(".theAnswer").removeAttr("style");
-    });
-
-    for (var i = 0; i < deck.length; i++ ) {
-      
-    }
-
+    $("#nextCard").on("click", function() {
+      index++;
+      showQnA(index);
+    })
 });
+
+var flashCards = [
+  {
+    question: "What should go at the top of every html document?",
+    answer: "The <!DOCTYPE html> tag"
+  },
+  {
+    question: "CSS is used to...",
+    answer: "Style the elements of a web page"
+  },
+  {
+    question: "JQuery is an example of a what?",
+    answer: "Javascript library"
+  }
+]
+
+function showQnA(index) {
+  var questionAndAnswer = flashCards[index];
+  var question = questionAndAnswer.question;
+  var answer = questionAndAnswer.answer;
+
+  $(".theQuestion p").html(question);
+  $(".theAnswer p").html(answer);
+}
